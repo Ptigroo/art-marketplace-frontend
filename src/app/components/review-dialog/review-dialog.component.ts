9d02859cca617c92496920ae145dfcdf6acf0992
Ptigroo <gaspard.villenfagne@gmail.com>
2025-05-30 18:15:39 +0200

    Review popup

---
+++ src/app/components/review-dialog/review-dialog.component.ts
@@ -0,0 +1,46 @@
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-review-dialog',
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RouterModule,
+    ReactiveFormsModule,
+  MatIconModule,
+    FormsModule,
+MatDialogModule],
+  templateUrl: './review-dialog.component.html',
+})
+export class ReviewDialogComponent {
+  reviewText = '';
+  rating: any = 0;
+
+  constructor(
+    public dialogRef: MatDialogRef<ReviewDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  submitReview() {
+    this.dialogRef.close({
+      review: this.reviewText,
+      rating: this.rating
+    });
+  }
+  
+  rateProduct(rating: number) {
+    this.rating = rating;
+  }
+}
--- src/app/pages/customer/my-products/my-products.page.ts
+++ src/app/pages/customer/my-products/my-products.page.ts
@@ -8,7 +8,9 @@ import { MatInputModule } from '@angular/material/input';
 import { RouterModule } from '@angular/router';
 import { ProductService } from '../../../services/product/product.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { ReviewDialogComponent } from '../../../components/review-dialog/review-dialog.component';
 @Component({
   selector: 'app-my-products',
   imports: [
@@ -18,13 +20,14 @@ import { MatSnackBar } from '@angular/material/snack-bar';
     MatFormFieldModule,
     MatInputModule,
     RouterModule,
-    ReactiveFormsModule],
+    ReactiveFormsModule,
+  MatIconModule],
   templateUrl: './my-products.page.html',
   styleUrl: './my-products.page.scss'
 })
 export class MyProductsPage {
  products: any[] = [];
-  constructor(private productService: ProductService,private snackBar: MatSnackBar) {}
+  constructor(private productService: ProductService,private snackBar: MatSnackBar, private dialog: MatDialog) {}
   
   ngOnInit(): void {
     this.productService.getBoughtProducts().subscribe({
@@ -32,4 +35,16 @@ export class MyProductsPage {
       error: err => console.error('Failed to load products:', err)
     });
   }
+  
+ openReviewDialog(product: any) {
+  const dialogRef = this.dialog.open(ReviewDialogComponent, {
+    width: '400px',
+    data: { product }
+  });
+  dialogRef.afterClosed().subscribe(result => {
+    if (result) {
+      // Call backend API to save review
+    }
+  });
+}
 }
